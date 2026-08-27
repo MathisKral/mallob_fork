8c77c81d987efde85c50ff28bfd6092bb216108a
Mathis Kral <mathis.kral@mail.de>
2026-08-27 09:06:46 -0400

    Update benchmarks to 2021 and add 'unsolvable' instance

--- PdF/01_prestudy/get_benchmarks.sh
+++ PdF/01_prestudy/get_benchmarks.sh
@@ -1,11 +1,19 @@
 dir="./PdF/01_prestudy"
+dir_esc=".\/PdF\/01_prestudy"
 
 # clrear directories
-rm ${dir}/benchmarks/*
-rm ${dir}/benchmarks_hard/*
+if [ $(ls ${dir}/benchmarks | wc -l) -gt 0 ]; then
+    rm -r ${dir}/benchmarks/*
+fi
 
-wget -P ${dir}/benchmarks --content-disposition -i ${dir}/track_main_2026.uri
-wget -P ${dir}/benchmarks_hard https://raw.githubusercontent.com/Byt-wyze-technology/sat-instances-library/refs/heads/main/instances/sat-Hard-C-445935conflicts-transition.cnf
+if [ $(ls ${dir}/benchmarks_hard | wc -l) -gt 0 ]; then
+    rm -r ${dir}/benchmarks_hard/*
+fi
+
+wget -P ${dir}/benchmarks --content-disposition -i ${dir}/main_benchmarks_2021.uri
+
+# get the "unsolvable" instance (mallob timeout in competition results)
+wget -P ${dir}/benchmarks_hard --content-disposition https://benchmark-database.de/file/1dce69ee6685597d6c56e5fd7a47f8e0
 
 # unpack benchmarks
 # for file in ${dir}/benchmarks/*; do
@@ -15,4 +23,8 @@ wget -P ${dir}/benchmarks_hard https://raw.githubusercontent.com/Byt-wyze-techno
 # write benchmarkfiles
 ls ${dir}/benchmarks > ${dir}/templates/benchmarks.txt.0
 ls ${dir}/benchmarks_hard > ${dir}/templates/benchmarks.txt.1
+
+# add file paths to benchmarkfiles
+sed -i 's/^/'${dir_esc}'\/benchmarks\//' ${dir}/templates/benchmarks.txt.0
+sed -i 's/^/'${dir_esc}'\/benchmarks_hard\//' ${dir}/templates/benchmarks.txt.1
 
