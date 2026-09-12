5fa6a09f2133527f26d6c62d87e0b5255aaec136
Mathis Kral <mathis.kral@mail.de>
2026-09-12 15:37:13 +0000

    Update sbatch file to run all 3 jobs at once

--- PdF/01_prestudy/run_sbatch.sh
+++ PdF/01_prestudy/run_sbatch.sh
@@ -3,22 +3,31 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=16
 #SBATCH --ntasks-per-core=2
+#SBATCH -J mallob-malleable-benchmark
+
+# log number of ranks
+echo "#ranks: $SLURM_NTASKS"
+
+dir="./PdF/01_prestudy"
+nbenchmarks="$(cat ${dir}/templates/benchmarks.txt.0 | wc -l)"
+
+# ===============
+# 50% disturbance
+# ===============
 
 #configuration
-dir="./PdF/01_prestudy"
-results_dir="${dir}/results"
-nbenchmarks="$(cat ${dir}/templates/benchmarks.txt.0 | wc -l)"
+results_dir="${dir}/results/${SLURM_JOB_ID}_results_50"
 
 # command
 cmd="
 `# run mallob with 2 clients` build/mallob -c=2 \
-`# templates for the 2 clients` -client-template=${dir}/templates/client-template.json \
+`# templates for the 2 clients` -client-template=${dir}/templates/client-template-50.json \
 `# job templates` -job-template=${dir}/templates/job-template.json \
 `# job descriptions` -job-desc-template=${dir}/templates/benchmarks.txt \
 `# active jobs per client` -ajpc=1 \
 `# loaded jobs per client` -ljpc=8 \
 `# job wallclock limit` -jwl=300 \
-`# threads per process` -t=2 \
+`# threads per process` -t=16 \
 `# finish only when all benchmarks are finidhed` -SJ=$nbenchmarks \
 `# shuffle job descriptions` -sjd=1 \
 `# portfolio` -satsolver=c
@@ -27,10 +36,70 @@ cmd="
 `# verbosity` -v=2
 "
 
-# clear results dir
-rm -r ${dir}/results/*
-
-echo JOB_LAUNCHING
+echo "launching mallob with 50% disturbance"
 echo $cmd
 mpirun -n $SLURM_NTASKS --bind-to core --map-by numa $cmd
-echo JOB_FINISHED
+echo "finished mallob with 50% disturbance"
+
+
+# ===============
+# 75% disturbance
+# ===============
+
+#configuration
+results_dir="${dir}/results/${SLURM_JOB_ID}_results_75"
+
+# command
+cmd="
+`# run mallob with 2 clients` build/mallob -c=2 \
+`# templates for the 2 clients` -client-template=${dir}/templates/client-template-75.json \
+`# job templates` -job-template=${dir}/templates/job-template.json \
+`# job descriptions` -job-desc-template=${dir}/templates/benchmarks.txt \
+`# active jobs per client` -ajpc=1 \
+`# loaded jobs per client` -ljpc=8 \
+`# job wallclock limit` -jwl=300 \
+`# threads per process` -t=16 \
+`# finish only when all benchmarks are finidhed` -SJ=$nbenchmarks \
+`# shuffle job descriptions` -sjd=1 \
+`# portfolio` -satsolver=c
+`# output file` -sro=${results_dir}/results.json \
+`# log dir` -log=${results_dir} \
+`# verbosity` -v=2
+"
+
+echo "launching mallob with 75% disturbance"
+echo $cmd
+mpirun -n $SLURM_NTASKS --bind-to core --map-by numa $cmd
+echo "finished mallob with 57% disturbance"
+
+
+
+# ===============
+# 0% disturbance
+# ===============
+
+#configuration
+results_dir="${dir}/results/${SLURM_JOB_ID}_results_00"
+
+# command
+cmd="
+`# run mallob with 1 client` build/mallob -c=1 \
+`# templates for the 2 clients` -client-template=${dir}/templates/client-template-00.json \
+`# job templates` -job-template=${dir}/templates/job-template.json \
+`# job descriptions` -job-desc-template=${dir}/templates/benchmarks.txt \
+`# active jobs per client` -ajpc=1 \
+`# loaded jobs per client` -ljpc=8 \
+`# job wallclock limit` -jwl=300 \
+`# threads per process` -t=16 \
+`# finish only when all benchmarks are finidhed` -SJ=$nbenchmarks \
+`# shuffle job descriptions` -sjd=1 \
+`# portfolio` -satsolver=c
+`# output file` -sro=${results_dir}/results.json \
+`# log dir` -log=${results_dir} \
+`# verbosity` -v=2
+"
+
+echo "launching mallob with 0% disturbance"
+echo $cmd
+mpirun -n $SLURM_NTASKS --bind-to core --map-by numa $cmd
+echo "finished mallob with 0% disturbance"
