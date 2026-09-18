0f44e429ec90f2e17f882b72146eeb42f4380833
Mathis Kral <mathis_tiberius.kral@mailbox.tu-dresden.de>
2026-09-18 17:04:41 +0200

    Update sbatch for prestudy disturbance benchmarks

---
+++ PdF/01_prestudy/README.txt
@@ -0,0 +1,17 @@
+# Prestudy Disturbance Benchmarks
+- benchmark from 2024 MallobSAT paper
+- BUT: slightly different run configurations
+
+## Run Configurations
+- 100% ressources undisturbed as baseline
+- 75% ressources undisturbed 
+- 50%-100% ressources disturbed 
+- 62.5% ressources undisturbed
+- 25%-100% ressources disturbed
+
+## What to compare
+- 75% undisturbed <-> 50%-100% disturbed (should ideally be equal)
+- 62.5% undisturbed <-> 25%-100% disturbed (should ideally be equal)
+
+## Benchmarks
+SAT competition 2021 → 400 benchmark
---
+++ PdF/01_prestudy/scripts/run_undisturb_62_5.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=120
+#SBATCH --ntasks-per-core=2  # use hyperthreading
+#SBATCH -J mallob-disturbance-62_5
+
+# ATTENTION: this benchmark should be performed on partition "diffie" or "hellman"
+
+# configuration
+dir="./PdF/01_prestudy"
+results_dir="${dir}/results/${SLURM_JOB_ID}_results_undisdurb_62_5"
+nbenchmarks="$(cat ${dir}/templates/benchmarks.txt.0 | wc -l)"
+job_time_limit=300
+time_limit=$(($nbenchmarks * ($job_time_limit * 0.85))) # assume a job takes an average time of 0.85*300s
+nprocs=40
+nthreads=3 # nprocs * nthreads = total cpus on one node
+
+
+# command
+cmd="
+`# run mallob with 1 client (undisturbed)` build/mallob -c=1 \
+`# templates for the 1/2 client(s)` -client-template=${dir}/templates/client-template-undisturbed.json \
+`# job templates` -job-template=${dir}/templates/job-template.json \
+`# job descriptions` -job-desc-template=${dir}/templates/benchmarks.txt \
+`# active jobs per client` -ajpc=1 \
+`# loaded jobs per client` -ljpc=8 \
+`# finish after n seconds` -T=${time_limit} \
+`# threads per process` -t=${nthreads} \
+`# shuffle job descriptions` -sjd=1 \
+`# portfolio` -satsolver=k_
+`# output file` -sro=${results_dir}/results.json \
+`# log dir` -log=${results_dir} \
+`# verbosity` -v=2 \
+`# dont wait for subprocesses to finish` -terminate-abruptly=1 \
+`# sat config directory` -sat-config-dirs=config/sat/base/ \
+`# preprocessor configuration` -preprocess-config=config/satwithpre/actors_onechain.json
+"
+
+# log
+echo "CONFIGURATION =============================="
+echo "#benchmarks: $nbenchmarks"
+echo "#nodes: $SLURM_NTASKS"
+echo "#processes: $nprocs"
+echo "#threads_per_process: $nthreads"
+echo "command: \n $cmd"
+echo "time limit: $time_limit seconds"
+
+# run job
+echo "LOGGING ===================================="
+mpirun -n $nprocs --bind-to core --map-by core $cmd
+
+echo "job finished"
